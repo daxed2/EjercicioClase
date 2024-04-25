1c26653b08114c3a0349d40bd8c128c439928ec4
daxed2 <158123332+daxed2@users.noreply.github.com>
2024-04-24 19:26:01 -0700

    Add project files.

---
+++ EjercicioClase/SuperHeroe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    public class SuperHeroe
+    {
+        public string Nombre { get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string Ciudad {  get; set; }        
+        public bool PuedeVolar {  get; set; }
+
+        public SuperPoder superPoder { get; set; }
+    }
+}
